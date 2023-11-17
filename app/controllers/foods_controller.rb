70a3cb01ba150e4bab3b69ea413d72c8913af847
Gardimy Charles <gardimycharles889@gmail.com>
2023-11-17 09:27:27 -0500

    Fix rubocop issue

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -27,24 +27,42 @@ class FoodsController < ApplicationController
     @general_food_list = current_user.foods
     @missing_food_items = []
 
-    @recipes.each do |recipe|
-      recipe.recipe_foods.each do |recipe_food|
-        general_food = @general_food_list.find_by(id: recipe_food.food_id)
-        next unless general_food.nil? || general_food.quantity < recipe_food.quantity
-
-        quantity_needed = recipe_food.quantity - (general_food.nil? ? 0 : general_food.quantity)
-        price = recipe_food.food.price * quantity_needed
-
-        @missing_food_items << {
-          food_name: recipe_food.food.name,
-          quantity_needed:,
-          price:
-        }
-      end
-    end
+    process_missing_food_items
 
     @total_food_items = @missing_food_items.sum { |item| item[:quantity_needed] }
     @total_price = @missing_food_items.sum { |item| item[:price] }
+  end
+
+  private
+
+  def process_missing_food_items
+    @recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        process_recipe_food(recipe_food)
+      end
+    end
+  end
+
+  def process_recipe_food(recipe_food)
+    general_food = @general_food_list.find_by(id: recipe_food.food_id)
+    return if general_food_exists_and_has_enough_quantity?(recipe_food, general_food)
+
+    quantity_needed = calculate_quantity_needed(recipe_food, general_food)
+    price = recipe_food.food.price * quantity_needed
+
+    @missing_food_items << {
+      food_name: recipe_food.food.name,
+      quantity_needed:,
+      price:
+    }
+  end
+
+  def general_food_exists_and_has_enough_quantity?(recipe_food, general_food)
+    !general_food.nil? && general_food.quantity >= recipe_food.quantity
+  end
+
+  def calculate_quantity_needed(recipe_food, general_food)
+    recipe_food.quantity - (general_food.nil? ? 0 : general_food.quantity)
   end
 
   def destroy
@@ -53,8 +71,6 @@ class FoodsController < ApplicationController
 
     redirect_to foods_url
   end
-
-  private
 
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
